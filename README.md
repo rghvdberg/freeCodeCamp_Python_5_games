fe8811ea16d269a0ef3faa2e627b03f68c2cb647
Rob van den Berg <rghvdberg@gmail.com>
2021-07-09 17:35:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Learn Python by Building Five Games  
+
+<https://youtu.be/XGf2GcyHPhc>  
+freeCodeCamp.org  
+
+Games:
+
+- [ ] Pong
+- [ ] Snake
+- [ ] Connect Four
+- [ ] Tetris
+- [ ] Online Multiplayer Game
---
+++ pong.py
@@ -0,0 +1,13 @@
+# Simple Pong in Python 3 for Beginners
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Main game loop
+while True:
+    wn.update()
