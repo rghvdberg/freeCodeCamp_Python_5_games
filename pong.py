5715d96570faed6893e8deea3ad34827f74a56b2
Rob van den Berg <rghvdberg@gmail.com>
2021-07-11 12:48:01 +0200

    add scores

--- pong.py
+++ pong.py
@@ -8,6 +8,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -37,7 +41,18 @@ ball.goto(0, 0)
 ball.dx = 0.05
 ball.dy = -0.05
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 # Function
+
+
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
@@ -96,11 +111,25 @@ while True:
         ball.goto(0, 0)
         # reverse direction
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(
+            "Player A: {}  Player B: {}".format(score_a, score_b),
+            align="center",
+            font=("Courier", 24, "normal"),
+        )
 
     # hit left side
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(
+            "Player A: {}  Player B: {}".format(score_a, score_b),
+            align="center",
+            font=("Courier", 24, "normal"),
+        )
 
     # paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (
@@ -109,7 +138,7 @@ while True:
         ball.dx *= -1
         ball.setx(340)
 
-    if (ball.xcor() < -340 and ball.xcor() > - 350) and (
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (
         ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50
     ):
         ball.dx *= -1
