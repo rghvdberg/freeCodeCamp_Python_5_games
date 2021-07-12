af352e6c834b6e89fa63e8604bb3a67b3207c69f
Rob van den Berg <rghvdberg@gmail.com>
2021-07-12 14:12:16 +0200

    snacks, grow snake, collision detection

--- snake.py
+++ snake.py
@@ -1,5 +1,4 @@
 # Snake Tutorial Python
-import math
 import random
 from turtle import circle
 import pygame
@@ -104,7 +103,12 @@ class snake(object):
                     c.move(c.dirnx, c.dirny)
 
     def reset(self, pos):
-        pass
+        self.head = cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
 
     def draw(self, surface):
         for i, c in enumerate(self.body):
@@ -112,6 +116,24 @@ class snake(object):
                 c.draw(surface, True)
             else:
                 c.draw(surface)
+
+    def addCube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
+
+        # determine position for new tail cube
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0] - 1, tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0] + 1, tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0], tail.pos[1] - 1)))
+
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0], tail.pos[1] + 1)))
+
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
 
 
 def drawGrid(width, rows, surface):
@@ -127,34 +149,66 @@ def drawGrid(width, rows, surface):
 
 
 def redrawWindow(surface):
-    global rows, width, s
+    global rows, width, s, snack
     surface.fill((0, 0, 0))
     s.draw(surface)
+    snack.draw(surface)
     drawGrid(width, rows, surface)
     pygame.display.update()
 
 
-def randomSnack(rows, items):
-    pass
+def randomSnack(rows, item):
+    # global rows
+    positions = item.body
+
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
+        else:
+            break
+    return (x, y)
 
 
 def message_box(subject, content):
-    pass
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
+
+    
 
 
 def main():
-    global width, rows, s
+    global width, rows, s, snack
     width = 500
     rows = 20
     win = pygame.display.set_mode((width, width))
     s = snake((255, 0, 0), (10, 10))
+    snack = cube(randomSnack(rows, s), color=(0, 255, 0))
     flag = True
     clock = pygame.time.Clock()
 
     while flag:
-        pygame.time.delay(50)
+        pygame.time.delay(200)
         clock.tick(10)
         s.move()
+        if s.body[0].pos == snack.pos:
+            s.addCube()
+            snack = cube(randomSnack(rows, s), color=(0, 255, 0))
+
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z: z.pos, s.body[x + 1 :])):
+                print("Score: ", len(s.body))
+                message_box("You Lost!", "Play Again...")
+                s.reset((10, 10))
+                break
+
         redrawWindow(win)
 
 
